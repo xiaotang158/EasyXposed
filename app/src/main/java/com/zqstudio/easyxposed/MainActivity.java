9b850bb366366b5017b65da767563319e4dea395
xiaotang158 <66454911+xiaotang158@users.noreply.github.com>
2024-07-11 20:31:16 +0800

    Update MainActivity.java

--- app/src/main/java/com/zqstudio/easyxposed/MainActivity.java
+++ app/src/main/java/com/zqstudio/easyxposed/MainActivity.java
@@ -8,25 +8,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	// @Override
+	// protected void onCreate(Bundle savedInstanceState) {
+	// 	super.onCreate(savedInstanceState);
+	// 	setContentView(R.layout.activity_main);
 
-		// 修改主页提示语
-		String msg = "EasyLSP已启用，点击关闭。\n并手动选择生效应用。\n开始你的 HOOK 吧！";
-		TextView textView = findViewById(R.id.tips);
-		textView.setText(msg);
-	}
+	// 	// 修改主页提示语
+	// 	String msg = "留白";
+	// 	TextView textView = findViewById(R.id.tips);
+	// 	textView.setText(msg);
+	// }
 
-	@Override
-	public boolean dispatchTouchEvent(MotionEvent ev) {
-		if (ev.getAction() == MotionEvent.ACTION_UP){
-			android.os.Process.killProcess(android.os.Process.myPid());
-		}else{
-			System.exit(0);
-		}
-		return super.dispatchTouchEvent(ev);
-	}
+	// @Override
+	// public boolean dispatchTouchEvent(MotionEvent ev) {
+	// 	if (ev.getAction() == MotionEvent.ACTION_UP){
+	// 		android.os.Process.killProcess(android.os.Process.myPid());
+	// 	}else{
+	// 		System.exit(0);
+	// 	}
+	// 	return super.dispatchTouchEvent(ev);
+	// }
 
 }
